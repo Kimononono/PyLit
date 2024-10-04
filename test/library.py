2ecc3295720c6a32dea4db5244e72d63270950b9
hunte <hunte@huntelap>
2024-10-04 00:45:55 -0500

    Added function books to /home/hunte/programming/messaround/smartdb/test/library.py

--- test/library.py
+++ test/library.py
@@ -3,12 +3,10 @@ from ..magicclass import MagicBaseClass
 class Library(MagicBaseClass):
     def __init__(self):
         pass
-        
-        self.books
     def existing_method(self):
         print("This is an existing method in the Library class.")
     
-    def new_method(self):
-        print('Function new_method was called')
+    def books(self):
+        print('Function books was called')
     
 
