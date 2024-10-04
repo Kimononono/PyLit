ae7d1101804f808cdc5a21c89182f87dacca44b1
hunte <hunte@huntelap>
2024-10-04 00:30:14 -0500

    Added function books to /home/hunte/programming/messaround/smartdb/test/library.py

--- test/library.py
+++ test/library.py
@@ -6,5 +6,9 @@ class Library(MagicBaseClass):
     
     def new_method(self):
         print('Function new_method was called')
+    def __init__():
+        
+        def books(self):
+            print('Function books was called')
     
 
